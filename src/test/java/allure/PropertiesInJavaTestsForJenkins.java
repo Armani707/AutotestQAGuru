76063d209dd1c8d3868c3dfafa79878f80361b9b
Arman <9armo9@mail.ru>
2026-01-07 22:43:44 +0300

    added notifications+Telegram

--- src/test/java/allure/PropertiesInJavaTestsForJenkins.java
+++ src/test/java/allure/PropertiesInJavaTestsForJenkins.java
@@ -3,6 +3,8 @@ package allure;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.String.format;
+
 public class PropertiesInJavaTestsForJenkins {
     @Test
     void systemPropertiesTest() {
@@ -45,6 +47,27 @@ public class PropertiesInJavaTestsForJenkins {
 
         // gradle property_test -Dbrowser=opera
         // opera
+    }
+
+    @Test
+    @Tag("hello")
+    void systemProperties5Test() {
+        String name = System.getProperty("name", "default student");
+        String message = format("Hello, %s!", name);
+
+        System.out.println(message);
+        // gradle hello_test
+        // Hello, default student!
+
+        // gradle hello_test -Dname=Alex Egorov
+        // BUILD FAILED: Task 'Egorov' not found in root project 'demoqa-tests-19'.
+
+        // gradle hello_test -Dname="Alex Egorov"
+        // gradle hello_test "-Dname=Alex Egorov"
+        // Hello, Alex Egorov!
+
+        //Есть настройка в build.gradle
+
     }
 
 }
